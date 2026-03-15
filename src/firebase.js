62ba7e34abc56d63bf25866c0b9732d5484f9331
DanteChoppaXXX <only1dantechoppa@gmail.com>
2026-03-15 16:56:55 +0100

    Essential Summary v1

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAsd7xfbl8Zv0Kdnky9VDUwdG1H70ZQAas",
+  authDomain: "essentialsummary.firebaseapp.com",
+  projectId: "essentialsummary",
+  storageBucket: "essentialsummary.firebasestorage.app",
+  messagingSenderId: "774222568892",
+  appId: "1:774222568892:web:b77f2f615783e1ede65fb9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
