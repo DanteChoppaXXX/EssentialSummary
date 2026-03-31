90a89295f1aa7774c38202d16b407d5ce2064ef3
DanteChoppaXXX <only1dantechoppa@gmail.com>
2026-03-31 08:03:49 +0100

    Essential Summary v2.0 (Added Google Analytics)

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
-  measurementId:meta.env.VITE_FIREBASE_MEASUREMENT_ID 
+  measurementId:import.meta.env.VITE_FIREBASE_MEASUREMENT_ID 
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
